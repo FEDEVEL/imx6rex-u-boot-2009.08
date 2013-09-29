240b2e8dc80d9b309cd48e9e0c266061b6cebee7
robertferanec <robert.feranec@fedevel.com>
2013-09-29 13:24:34 -0700

    Initial version

---
+++ build_u-boot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi- distclean
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi- mx6q_rex_config
+make ARCH=arm CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
+
